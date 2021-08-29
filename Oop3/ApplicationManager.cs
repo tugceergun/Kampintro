baeea523f15e976f16d6f957a18b7a37a0e18ee4
Tuğçe Ergün <tugceergun000@gmail.com>
2021-08-29 19:13:32 +0300

    Interface ve Logger

--- Oop3/ApplicationManager.cs
+++ Oop3/ApplicationManager.cs
@@ -7,12 +7,18 @@ namespace Oop3
     //başvuru
     class ApplicationManager
     {
-        public void Apply(ICreditManager creditManager) //herkesin referansını tutabilen ICreditManager'ı parametre yaptık.
-        {
+        //Method injection (Methodun kullanacağı kredi türünün ne olacağını ve hangi loglayıcı olacağını enjekte ediyoruz.)
+        public void Apply(ICreditManager creditManager,List<ILoggerService> loggerServices) //herkesin referansını tutabilen ICreditManager'ı parametre yaptık.
+        {                                                                                  //Loglama işlemini de ekledik.(birden fazla seçilebileceği için liste yaptık.)
             //başvuran bilgilerini değerlendirme
             //
 
             creditManager.Calculate();
+
+            foreach (var loggerService in loggerServices)  //birden fazla logger servisi seçebiliriz.
+            {
+                loggerService.Log();    //her bir loglayıcıyı çalıştır.
+            }
         }
 
         public void CreditPreInfo(List<ICreditManager> credits)  //başvuru ön bilgilendirmesi.//birçok kredi türü olabilir o yüzden ICreditManager türünde yaptık.
--- Oop3/Program.cs
+++ Oop3/Program.cs
@@ -7,14 +7,18 @@ namespace Oop3
     {
         static void Main(string[] args)
         {
-            ICreditManager consumerLoanManager = new ConsumerLoanManager();
+            ICreditManager consumerLoanManager = new ConsumerLoanManager(); //kredi çeşitleri.
             ICreditManager housingLoanManager = new HousingLoanManager();
             ICreditManager autoLoanManager = new AutoLoanManager();
             //AutoLoanManager yerine ICreditManager dedik.
             //Yani interface'ler de; o interface'i implemente eden classın referans numarasını tutabiliyor.
 
+            ILoggerService databeseLoggerServise = new DatabaseLoggerService();  //loglama.
+            ILoggerService fileLoggerService = new FileLoggerService();
+
             ApplicationManager applicationManager = new ApplicationManager();
-            applicationManager.Apply(consumerLoanManager);
+            applicationManager.Apply(consumerLoanManager,new List<ILoggerService>(){databeseLoggerServise,fileLoggerService, });
+            //hangi kredi çeşidinin olacağını ve hangi loglama servislerinin olacağını seçtik.
 
             List<ICreditManager> credits = new List<ICreditManager>() {consumerLoanManager,autoLoanManager}; //iki kredi de müşteriye uyuyor.
             applicationManager.CreditPreInfo(credits);
